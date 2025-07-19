8359d607c09d5894b043f5cfdb3abb074e269e3f
Jid Kimachia <kimachiajidraf@gmail.com>
2025-07-19 23:53:33 +0300

    registered jwt on program.cs

--- AuthPractice/Program.cs
+++ AuthPractice/Program.cs
@@ -1,11 +1,21 @@
+using AuthPractice.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//register jwt auth
+builder.Services.ConfigureJwt(builder.Configuration);
+
+//for auth to work
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +28,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
